0213e083c07d90d9298a3513fdf26bd15193a705
Sebastian Navarrete <sebasnavarrete.2@gmail.com>
2021-03-04 16:38:35 -0500

    coord. bug fix

--- venesperanza-back/app/Http/Controllers/LlegadasController.php
+++ venesperanza-back/app/Http/Controllers/LlegadasController.php
@@ -18,36 +18,31 @@ class LlegadasController extends Controller
      */
 
 
-    public function reportarllegada(Request $request){
+    public function reportarllegada(Request $request)
+    {
 
         try {
             $miembroHogar = MiembrosHogar::with(['encuesta'])
-            ->where('numero_documento','=',$request['formData']['numeroDocumentoCtrl'])
-            ->where('tipo_documento','=',$request['formData']['tipoDocumentoCtrl'])
-            ->first();
-
-            //$miembroHogar->tipo_documento = $request['tipoDocumentoCtrl'];
-            //$miembroHogar-> = $request['telefonoCtrl']
+                ->where('numero_documento', '=', $request['formData']['numeroDocumentoCtrl'])
+                ->where('tipo_documento', '=', $request['formData']['tipoDocumentoCtrl'])
+                ->first();
 
             $datosGuardados = null;
-            if($miembroHogar){
+            if ($miembroHogar) {
                 $llegada = new Llegadas;
-
                 $llegada->tipo_documento = $request['formData']['tipoDocumentoCtrl'];
                 $llegada->numero_documento = $request['formData']['numeroDocumentoCtrl'];
                 $llegada->id_departamento = $request['formData']['departamentoCtrl'];
                 $llegada->id_municipio = $request['formData']['municipioCtrl'];
                 $llegada->telefono = $request['formData']['telefonoCtrl'];
-    
                 $llegada->id_encuesta = $miembroHogar['encuesta']['id'];
-    
-                $llegada->latitud = $request['coordenadas']['latitud'];
-                $llegada->longitud = $request['coordenadas']['longitud'];
-
+                if ($request['coordenadas']) {
+                    $llegada->latitud = $request['coordenadas']['latitud'];
+                    $llegada->longitud = $request['coordenadas']['longitud'];
+                }
                 $llegada->save();
-
                 $datosGuardados = $llegada;
-            }else{
+            } else {
                 $intentos = new Intentos;
 
                 $intentos->tipo_documento = $request['formData']['tipoDocumentoCtrl'];
@@ -55,36 +50,21 @@ class LlegadasController extends Controller
                 $intentos->id_departamento = $request['formData']['departamentoCtrl'];
                 $intentos->id_municipio = $request['formData']['municipioCtrl'];
                 $intentos->telefono = $request['formData']['telefonoCtrl'];
-                $intentos->latitud = $request['coordenadas']['latitud'];
-                $intentos->longitud = $request['coordenadas']['longitud'];
+                if ($request['coordenadas']) {
+                    $intentos->latitud = $request['coordenadas']['latitud'];
+                    $intentos->longitud = $request['coordenadas']['longitud'];
+                }
 
                 $intentos->save();
 
                 $datosGuardados = $intentos;
             }
-            /*
-            $encuesta = Encuesta::where('id', '=', $miembroHogar['encuesta']['id'])->first();
-
-            $encuesta['tipo_documento'] = $request['formData']['tipoDocumentoCtrl'];
-            $encuesta['id_departamento'] = $request['formData']['departamentoCtrl'];
-            $encuesta['id_municipio'] = $request['formData']['municipioCtrl'];
-            $encuesta['numero_contacto'] = $request['formData']['telefonoCtrl'];
-            $encuesta['numero_documento'] = $request['formData']['numeroDocumentoCtrl'];*/
-
-            
-
-            /*$autorizacion = Autorizacion::where('id_encuesta','=',$encuesta['id'])->first();
-
-            $autorizacion->latitud = $request['coordenadas']['latitud'];
-            $autorizacion->longitud = $request['coordenadas']['longitud'];*/
-
-       
             if ($datosGuardados) {
                 return response()->json([
                     'res' => $datosGuardados->id,
                     //'token' => $token,
                     'message' => 'Llegada guardada'
-                ],200);
+                ], 200);
             } else {
                 return "error";
             }
@@ -111,7 +91,7 @@ class LlegadasController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -122,7 +102,7 @@ class LlegadasController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Llegadas  $llegadas
+     * @param \App\Models\Llegadas $llegadas
      * @return \Illuminate\Http\Response
      */
     public function show(Llegadas $llegadas)
@@ -133,7 +113,7 @@ class LlegadasController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Models\Llegadas  $llegadas
+     * @param \App\Models\Llegadas $llegadas
      * @return \Illuminate\Http\Response
      */
     public function edit(Llegadas $llegadas)
@@ -144,8 +124,8 @@ class LlegadasController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Llegadas  $llegadas
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Models\Llegadas $llegadas
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Llegadas $llegadas)
@@ -156,7 +136,7 @@ class LlegadasController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Llegadas  $llegadas
+     * @param \App\Models\Llegadas $llegadas
      * @return \Illuminate\Http\Response
      */
     public function destroy(Llegadas $llegadas)
