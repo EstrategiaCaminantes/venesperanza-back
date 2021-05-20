83f7ae7c8c137cef68a96cabc898900d77f46581
Sebastian Navarrete <sebasnavarrete.2@gmail.com>
2021-05-20 12:50:09 -0500

    bug fix migration

--- venesperanza-back/database/migrations/2021_05_14_203717_add_columnschatbot_to_encuesta_and_miembros_hogar_table.php
+++ venesperanza-back/database/migrations/2021_05_14_203717_add_columnschatbot_to_encuesta_and_miembros_hogar_table.php
@@ -20,18 +20,17 @@ class AddColumnschatbotToEncuestaAndMiembrosHogarTable extends Migration
             $table->tinyInteger('encuesta_chatbot')->nullable()->default(NULL); //Indica si el usuario empezó a responder la encuesta en el chatbot
             $table->integer('paso_chatbot')->nullable()->default(NULL); //Indica el paso en la encuesta del chatbot en el que está el usuario.
             $table->integer('pregunta')->nullable()->default(NULL); //Indica la pregunta en la encuesta del chatbot en el que está el usuario.
-            $table->integer('miembro_hogar_preguntando')->nullable()->default(NULL);  
+            $table->integer('miembro_hogar_preguntando')->nullable()->default(NULL);
             //Identifica para cuál Otro miembro de hogar se están respondiendo las preguntas del chatbot en el paso 2. Coincide con el campo chatbot_wapp_numero_miembro en la tabla ‘miembros_hogar’
 
-            $table->string('nombre_municipio_destino_final')->nullable()->default(NULL); 
+            $table->string('nombre_municipio_destino_final')->nullable()->default(NULL);
             //nombre del municipio de destino final en paso 1
-
-            DB::statement("ALTER TABLE `encuesta` CHANGE `paso` `paso` VARCHAR(20) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL;");
-            //cambiar campo 'paso' a NULL, este campo no tiene valor cuando se crea desde chatbot o kobo, 
+            $table->string('paso', 20)->nullable()->change();
+            //cambiar campo 'paso' a NULL, este campo no tiene valor cuando se crea desde chatbot o kobo,
         });
 
         Schema::table('miembros_hogar', function (Blueprint $table) {
-            $table->integer('numero_miembro')->nullable()->default(NULL); 
+            $table->integer('numero_miembro')->nullable()->default(NULL);
             // Sirve para identificar en el chatbot a cada uno de los Otros miembros de la familia que están con el encuestado-jefe de hogar. Y poder guardar la información correspondiente a cada miembro.
         });
     }
